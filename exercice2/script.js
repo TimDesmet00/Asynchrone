10ca57446c402074635bfb46fd3966c519ee7e49
tim <timdesmet00@gmail.com>
2023-10-09 16:59:26 +0200

    retravail de l'exo 2

--- exercice2/script.js
+++ exercice2/script.js
@@ -1,10 +1,14 @@
 const btn = document.getElementById("btnAutoDestruction");
 const name = document.getElementById("name");
 const pays = document.getElementById("pays");
+const answer = document.getElementById("response");
+
+let interedName;
+let interedPays;
 
 btn.addEventListener("click", () => {
-  const interedName = name.value;
-  const interedPays = pays.value;
+  interedName = name.value;
+  interedPays = pays.value;
 
   const fetchName = (interedName, interedPays) =>
     fetch(`https://api.agify.io?name=${interedName}&country_id=${interedPays}`);
@@ -14,8 +18,26 @@ btn.addEventListener("click", () => {
     .then((json) => {
       console.log(json.age);
       console.log(json.count);
+      insertAnswer(json);
+      localStor(json);
     })
     .catch((error) => {
       console.log("There was an error!", error);
     });
 });
+
+const insertAnswer = (json) => {
+  let ageAnswer = document.createElement("p");
+  ageAnswer.textContent = `age moyen pour ${interedName}: ${json.age}`;
+
+  let countAnswer = document.createElement("p");
+  countAnswer.textContent = `nombre de personne: ${json.count}`;
+
+  answer.appendChild(ageAnswer);
+  answer.appendChild(countAnswer);
+};
+
+const localStor = (json) => {
+  localStorage.setItem("age", json.age);
+  localStorage.setItem("number", json.count);
+};
